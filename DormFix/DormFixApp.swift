10eae4b5952fcfef715c8c6dd4c43a3fdebff02b
Setthawut Sikhiao <6731503126@lamduan.mfu.ac.th>
2026-02-13 14:31:22 +0700

    Initial Commit

---
+++ DormFix/DormFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  DormFixApp.swift
+//  DormFix
+//
+//  Created by Macsy💕 on 13/2/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct DormFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
